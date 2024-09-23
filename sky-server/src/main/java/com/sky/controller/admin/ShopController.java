94033ff8a99755959763406e2c03e05ebeb40199
雨不丘 <12752591+kingsyuqiu@user.noreply.gitee.com>
2024-09-23 09:22:52 +0800

    feat : 店铺状态存储和获取

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,45 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.KeyConstant;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping("/admin/shop")
+@Api("管理端店铺管理")
+public class ShopController {
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 修改店铺状态
+     * @param status
+     * @return
+     */
+    @ApiOperation("修改店铺状态")
+    @PutMapping("/{status}")
+    public Result status(@PathVariable String status){
+        redisTemplate.opsForValue().set(KeyConstant.SHOP_STATUS_KEY, status);
+        // TODO: 这玩意配置怎么读取的？
+        return Result.success();
+    }
+
+    /**
+     * 查询店铺状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("查询店铺状态")
+    public Result getStatus(){
+        Object status = redisTemplate.opsForValue().get(KeyConstant.SHOP_STATUS_KEY);
+        return Result.success(status);
+    }
+}
---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,34 @@
+package com.sky.controller.user;
+
+import com.sky.constant.KeyConstant;
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping("/user/shop")
+@Api("用户端店铺管理")
+public class ShopController {
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取店铺状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺状态")
+    public Result getStatus(){
+        Object status = redisTemplate.opsForValue().get(KeyConstant.SHOP_STATUS_KEY);
+        return Result.success(status);
+    }
+}
